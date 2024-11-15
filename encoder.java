574ba6dec95ec993e79700749e61a55179346913
Tsintao-9 <declanc@quadprep.org>
2024-11-15 11:15:29 -0500

    Add files via upload

---
+++ encoder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class encoder {
+	public static void main(String []args){
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("hi");
+		String message=Scanner.nextline();
+		int key=scanner.nextInt();
+		String encode="";
+		for(int=0;0;i<message.;i=+) {
+			char letter=message.
+			char encodedLetter=(char)(letter+key)
+			encode +=encodedLetter;
+		System.outprintln(encode;)
+	scanner.close();
+}}
